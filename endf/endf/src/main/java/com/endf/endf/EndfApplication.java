15a5fcfb6adb8079b310cbe3da719c7831ddd21a
Lucas Montesino Alves <lmalves.new@gmail.com>
2026-07-28 18:59:44 -0300

    mapeando backend

---
+++ endf/endf/src/main/java/com/endf/endf/EndfApplication.java
@@ -0,0 +1,13 @@
+package com.endf.endf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndfApplication.class, args);
+	}
+
+}
---
+++ endf/endf/src/test/java/com/endf/endf/EndfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endf.endf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
