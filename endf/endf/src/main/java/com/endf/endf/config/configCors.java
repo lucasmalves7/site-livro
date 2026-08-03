e63e679311f35cb0599c9e0eca0164d3c915735b
Lucas Montesino Alves <lmalves.new@gmail.com>
2026-08-03 20:00:15 -0300

    configurando backend

---
+++ endf/endf/src/main/java/com/endf/endf/config/configCors.java
@@ -0,0 +1,35 @@
+package com.endf.endf.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5500",
+                                "http://127.0.0.1:5500",
+                                "https://SEU-USUARIO.github.io"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+
+            }
+
+        };
+
+    }
+
+}
